048901b5d5740d06c94a8fb9792cf7f2fae923f0
irruptor <samirul.haque.rnc@gmail.com>
2017-03-26 00:32:51 +0530

    Role and perm unit testcases

---
+++ authz/authz-cmd/src/test/java/com/att/cmd/perm/ListByNsTest.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2017 AT&T Intellectual Property. All rights reserved.
+ *******************************************************************************/
+package com.att.cmd.perm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.att.cadi.CadiException;
+import com.att.cadi.LocatorException;
+import com.att.cmd.AAFcli;
+import com.att.cmd.AAFcliTest;
+import com.att.cmd.role.Role;
+import com.att.inno.env.APIException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ListByNsTest {
+	
+	private static ListByNS lsByNS;
+	
+	@BeforeClass
+	public static void setUp () throws NoSuchFieldException, SecurityException, Exception, IllegalAccessException {
+		AAFcli cli = AAFcliTest.getAAfCli();
+		Role role = new Role(cli);
+		Perm perm = new Perm(role);
+		List ls = new List(perm);
+		lsByNS = new ListByNS(ls);
+	}
+	
+	@Test
+	public void exec() {
+		try {
+			assertEquals(lsByNS._exec(0, "add","del","reset","extend"),500);
+		} catch (CadiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (APIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LocatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
